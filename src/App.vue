4117f5a5cc2da6a21383bec69a061f8e41ca4f5e
chenuvi <chenuvi@outlook.com>
2020-06-15 00:27:30 +0800

    style:header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
-
+    <Header></Header>
   </div>
 </template>
+
+<script>
+  import Header from "./components/Header.vue"
+  export default {
+    name:'App',
+    components:{
+      Header
+    }
+  }
+</script>
 
 <style>
 
