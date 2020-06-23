954da2c81c92d71d8869fa99983095f62f52506f
chenuvi <chenuvi@outlook.com>
2020-06-23 22:35:24 +0800

    feat:postlist.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div id="app">
     <Header></Header>
-    <Postlist></Postlist>
+    <div class="main">
+      <Postlist></Postlist>
+    </div>
   </div>
 </template>
 
 <script>
-  import Header from "./components/Header.vue"
-  import Postlist from "./components/Postlist"
-  export default {
-    name:'App',
-    components:{
-      Header,
-      Postlist
-    }
+import Header from "./components/Header.vue";
+import Postlist from "./components/Postlist";
+export default {
+  name: "App",
+  components: {
+    Header,
+    Postlist
   }
+};
 </script>
 
-<style>
-
+<style  >
+.main{
+  width:80%;
+}
 </style>
--- src/components/Postlist.vue
+++ src/components/Postlist.vue
@@ -2,13 +2,33 @@
 <template>
   <div>
     <div class="loading" v-if="isLoading">
-      <img src="../assets/loading.gif">
+      <img src="../assets/loading.gif" />
     </div>
     <div class="posts" v-else>
       <ul>
-<!--        头像-->
-        <li v-for="post in posts" class="PostList" :key="post.author.avatar_url">
-          <img :src="post.author.avatar_url" alt="头像">
+        <div class="toobar">
+          <span>全部</span>
+          <span>精华</span>
+          <span>分享</span>
+          <span>问答</span>
+          <span>招聘</span>
+        </div>
+        <li v-for="post in posts" class="PostList" :key="post in posts">
+          <!--头像-->
+          <img :src="post.author.avatar_url" alt="头像" />
+          <!-- 帖子的分类 -->
+          <span
+            :class="[{put_good:(post.good === true),put_top:(post.top === true),
+          'topiclist-tab':(post.good !== true&&post.top !== true)}]">
+            <span>{{post | tabFormatter}}</span>
+          </span>
+          <!-- 回复/浏览量 -->
+          <span>{{post.reply_count}}/{{post.visit_count}}</span>
+          <span>{{post.title}}</span>
+          <!-- 最终时间 -->
+          <span class="last_reply">
+            {{post.last_reply_at | formatDate}}
+          </span>
         </li>
       </ul>
     </div>
@@ -16,156 +36,161 @@
 </template>
 
 <script>
-  export default {
-    name: "Postlist",
-    data() {
-      return {
-        isLoading: false,
-        posts:[],
-        postpage:1
-      }
-    },
-    methods:{
-      getData(){
-        this.$http.get('https://cnodejs.org/api/v1/topics',{
-          params:{
-            page:this.postpage,
-            limit:30
+export default {
+  name: "Postlist",
+  data() {
+    return {
+      isLoading: false,
+      posts: [],
+      postpage: 1
+    };
+  },
+  methods: {
+    getData() {
+      this.$http
+        .get("https://cnodejs.org/api/v1/topics", {
+          params: {
+            page: this.postpage,
+            limit: 20
           }
         })
-          .then(res => {
-            this.isLoading = false;
-            this.posts = res.data.data
-          })
-          .catch(function (err) {
-            console.log(err)
-          })
-      }
-    },
-    beforeMount() {
-      this.isLoading = true
-      this.getData()
+        .then(res => {
+
+          this.isLoading = false;
+          this.posts = res.data.data;
+          // console.log('this.posts: ', this.posts);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
     }
+  },
+  beforeMount() {
+    this.isLoading = true;
+    this.getData();
   }
+};
 </script>
 
 <style scoped>
-  .PostList{
-    background-color: #e1e1e1;
-  }
-  .posts {
-    margin-top: 10px;
-  }
+.PostList {
+  background-color: #e1e1e1;
+}
+.posts {
+  margin-top: 10px;
+}
 
-  .PostList img {
-    height: 30px;
-    width: 30px;
-    vertical-align: middle;
-  }
+.PostList img {
+  height: 30px;
+  width: 30px;
+  vertical-align: middle;
+}
 
-  ul {
-    list-style: none;
-    width: 100%;
-    max-width: 1344px;
-    margin: 0 auto;
-  }
+ul {
+  list-style: none;
+  width: 100%;
+  max-width: 1344px;
+  margin: 0 auto;
+}
 
-  ul li:not(:first-child) {
-    padding: 9px;
-    font-size: 15px;
-    font-family: "Helvetica Neue", "Luxi Sans", "DejaVu Sans", Tahoma, "Hiragino Sans GB", STHeiti, sans-serif !important;
-    font-weight: 400;
-    background-color: white;
-    color: #333;
-    border-top: 1px solid #f0f0f0;
-  }
+ul li:not(:first-child) {
+  padding: 9px;
+  font-size: 15px;
+  font-family: "Helvetica Neue", "Luxi Sans", "DejaVu Sans", Tahoma,
+    "Hiragino Sans GB", STHeiti, sans-serif !important;
+  font-weight: 400;
+  background-color: white;
+  color: #333;
+  border-top: 1px solid #f0f0f0;
+}
 
-  li:not(:first-child):hover {
-    background: #f5f5f5;;
-  }
+li:not(:first-child):hover {
+  background: #f5f5f5;
+}
 
-  li:last-child:hover {
-    background: white;
-  }
+li:last-child:hover {
+  background: white;
+}
 
-  li span {
-    line-height: 30px;
-  }
+li span {
+  line-height: 30px;
+}
 
-  .allcount {
-    width: 70px;
-    display: inline-block;
-    text-align: center;
-    font-size: 12px;
-  }
+.allcount {
+  width: 70px;
+  display: inline-block;
+  text-align: center;
+  font-size: 12px;
+}
 
-  .reply_count {
-    color: #9e78c0;
-    font-size: 14px;
-  }
+.reply_count {
+  color: #9e78c0;
+  font-size: 14px;
+}
 
-  .put_good, .put_top {
-    background: #80bd01;
-    padding: 2px 4px;
-    border-radius: 3px;
-    -webkit-border-radius: 3px;
-    -moz-border-radius: 3px;
-    -o-border-radius: 3px;
-    color: #fff;
-    font-size: 12px;
-    margin-right: 10px;
-  }
+.put_good,
+.put_top {
+  background: #80bd01;
+  padding: 2px 4px;
+  border-radius: 3px;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  -o-border-radius: 3px;
+  color: #fff;
+  font-size: 12px;
+  margin-right: 10px;
+}
 
-  .topiclist-tab {
-    background-color: #e5e5e5;
-    color: #999;
-    padding: 2px 4px;
-    border-radius: 3px;
-    -webkit-border-radius: 3px;
-    -moz-border-radius: 3px;
-    -o-border-radius: 3px;
-    font-size: 12px;
-    margin-right: 10px;
-  }
+.topiclist-tab {
+  background-color: #e5e5e5;
+  color: #999;
+  padding: 2px 4px;
+  border-radius: 3px;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  -o-border-radius: 3px;
+  font-size: 12px;
+  margin-right: 10px;
+}
 
-  .last_reply {
-    text-align: right;
-    min-width: 50px;
-    display: inline-block;
-    white-space: nowrap;
-    float: right;
-    color: #778087;
-    font-size: 12px;
-  }
+.last_reply {
+  text-align: right;
+  min-width: 50px;
+  display: inline-block;
+  white-space: nowrap;
+  float: right;
+  color: #778087;
+  font-size: 12px;
+}
 
-  .toobar {
-    height: 40px;
-    background-color: #f5f5f5;
-  }
+.toobar {
+  height: 40px;
+  background-color: #f5f5f5;
+}
 
-  .toobar span {
-    font-size: 14px;
-    color: #80bd01;
-    line-height: 40px;
-    margin: 0 10px;
-    cursor: pointer;
-  }
+.toobar span {
+  font-size: 14px;
+  color: #80bd01;
+  line-height: 40px;
+  margin: 0 10px;
+  cursor: pointer;
+}
 
-  .toobar span:hover {
-    color: #9e78c0;
-  }
+.toobar span:hover {
+  color: #9e78c0;
+}
 
-  a {
-    text-decoration: none;
-    color: black;
-  }
+a {
+  text-decoration: none;
+  color: black;
+}
 
-  a:hover {
-    text-decoration: underline;
-  }
+a:hover {
+  text-decoration: underline;
+}
 
-  .loading {
-    text-align: center;
-    padding-top: 300px;
-  }
+.loading {
+  text-align: center;
+  padding-top: 300px;
+}
 </style>
